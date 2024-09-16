7259c163cb982f570f0cd4b3d1e2975442550aaf
mielnnik <nikola.milenkovic@outlook.com>
2024-09-16 22:29:43 +0200

    add octo

--- init.lua
+++ init.lua
@@ -920,6 +920,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.harpoon',
   require 'custom.plugins.hop',
+  require 'custom.plugins.octo',
+
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
